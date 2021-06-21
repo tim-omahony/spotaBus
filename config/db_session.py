0549b1510d0ffb005cadcddc7662e8d605f208af
Tim <timothyomahony@gmail.com>
2021-06-21 16:38:05 +0100

    fixed weather functionality with proper connection to DB

--- config/db_session.py
+++ config/db_session.py
@@ -5,7 +5,7 @@ environment = os.environ.get('DJANGO_ENV') or 'development'
 if environment == 'development':
     from config.db_details import *
 else:
-    from config.db_details_prod import *
+    from config.db_details import *
 
 engine = create_engine("mysql+pymysql://{}:{}@{}:{}/{}".format(USER, PASSWORD, URI, PORT, DB), echo=True)
 
