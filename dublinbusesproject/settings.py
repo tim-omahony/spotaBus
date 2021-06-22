2fca1a0c7da930c8e53d9e69d97fa11294df0dc9
David Kuhestani <david.kuhestani@ucdconnect.ie>
2021-06-22 22:21:20 +0200

    adding react project

--- dublinbusesproject/settings.py
+++ dublinbusesproject/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'dublinbusapplication.apps.DublinbusapplicationConfig'
+    'dublinbusapplication.apps.DublinbusapplicationConfig',
+    'rest_framework',
+    'frontend.apps.FrontendConfig'
 ]
 
 MIDDLEWARE = [
