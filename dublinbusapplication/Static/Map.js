b3238c6d7927b15acb2c6ec2c4f8d0e914c67b43
Tim <timothyomahony@gmail.com>
2021-06-24 23:33:15 +0100

    added a "data" directory with stop info. pushed all stop info to the RDS database :)

--- dublinbusapplication/Static/Map.js
+++ dublinbusapplication/Static/Map.js
@@ -1,14 +1,14 @@
-var DUBLIN_LAT = 53.349804;
-var DUBLIN_LNG = -6.260310;
+const DUBLIN_LAT = 53.349804;
+const DUBLIN_LNG = -6.260310;
 
-var DUBLIN_BOUNDS = {
-            north: 53.4,
-            south: 53.3,
-            west: -6.3103,
-            east: -6.2305,
+const DUBLIN_BOUNDS = {
+    north: 53.4,
+    south: 53.3,
+    west: -6.3103,
+    east: -6.2305,
 };
 
-var map;
+let map;
 
 function initMap() {
     map = new google.maps.Map(document.getElementById("MapView"), {
