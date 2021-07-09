a7de0519a8732ca3577416d503f3b8ff8cad4e19
David Kuhestani <david.kuhestani@ucdconnect.ie>
2021-07-09 12:14:56 +0200

    added login feature

--- dublinbusapplication/urls.py
+++ dublinbusapplication/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
 from . import views
+import dublinbusapplication
 
 urlpatterns = [
-    path('', views.index),
+    path('', views.index, name="home"),
     path('stops/', views.stops),
     path('about/', views.about),
     path('contact/', views.contact),
-    path('search_test/', views.search_test)
+    path('search_test/', views.search_test),
+    path('login/', views.loginPage, name="login"),
+    path('logout/', views.logoutUser, name="logout"),
+    path('register/', views.registerPage, name="register")
 ]
--- dublinbusapplication/views.py
+++ dublinbusapplication/views.py
@@ -1,8 +1,11 @@
 import json
 
 from django.http import HttpResponse, JsonResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.core import serializers
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
 
 from .models import Stop
 
@@ -19,11 +22,56 @@ def stops(request):
     stops = Stop.objects.all().values()
     return JsonResponse({'stops': list(stops)})
 
+
 def about(request):
     return render(request, 'about.html')
+
 
 def contact(request):
     return render(request, 'contact.html')
 
+
 def search_test(request):
     return render(request, 'search_test.html')
+
+
+def registerPage(request):
+    if request.user.is_authenticated:
+        return redirect('home')
+    else:
+        form = UserCreationForm()
+        if request.method == 'POST':
+            form = UserCreationForm(request.POST)
+            if form.is_valid():
+                form.save()
+                user = form.cleaned_data.get('username')
+                messages.success(request, "Account was created for " + user)
+                return redirect('login')
+
+        context = {'form': form}
+        return render(request, 'register.html', context)
+
+
+def loginPage(request):
+    if request.user.is_authenticated:
+        return redirect('home')
+    else:
+        if request.method == 'POST':
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('home')
+            else:
+                messages.info(request, 'Username or Password is incorrect.')
+
+    context = {}
+    return render(request, 'login.html', context)
+
+
+def logoutUser(request):
+    logout(request)
+    return redirect('login')
+
