ea579f77790084f81172b026ee5222f414680264
CianFitzgerald <cian.fitzgerald97@gmail.com>
2021-07-26 21:56:31 +0100

    Cian/ added test pickles for each route updated get prediction script with new features and fucntions added toggle buttons

--- dublinbusapplication/predictive_model/get_prediction.py
+++ dublinbusapplication/predictive_model/get_prediction.py
@@ -14,20 +14,55 @@ def filePath(filename):
 # end_stop_id = 114
 # temp = 6
 # weather_main = 'Rain'
-stops = [1730, 1731, 1732, 1733, 1650, 1652, 5141, 1773, 1774, 1651, 4784, 608, 4791, 4792, 1196,
-         1197, 4473, 250, 251, 4597, 255, 218, 219, 220, 221, 222, 223, 224, 225, 226, 114, 1645, 1646, 7571]
+
+# currently the route and stops sequence dictionary are hard coded
+# but these will be retrieved from the google response and a static JSON file
+
+route = 104
+stops_dict = {'d1': [1730, 1731, 1732, 1733, 1650, 1652, 5141, 1773, 1774, 1651, 4784, 608, 4791, 4792, 1196,
+                     1197, 4473, 250, 251, 4597, 255, 218, 219, 220, 221, 222, 223, 224, 225, 226, 114, 1645, 1646,
+                     7571],
+              'd2': [7571, 1644, 1605, 228, 229, 227, 230, 231, 232, 233, 242, 243, 253, 245, 4474, 4790, 1220, 1221,
+                     674,
+                     4389, 4390, 530, 4785, 1764, 1765, 1766, 1767, 7129, 1744, 1745, 1746, 1747, 1748, 1749]}
 
 
-def prediction(hour, day, month, start_stop_id, end_stop_id, temp, weather_main, stops):
-    journey_stops = stops[stops.index(start_stop_id):stops.index(end_stop_id) + 1]
+def peak_hour(hour):
+    """ returns 1 if hour is within peak hour time slots, 0 otherwise"""
+    if (7 < hour < 10) or (16 < hour < 19):
+        peak = 1
+    else:
+        peak = 0
 
-    prediction_df = pd.DataFrame(journey_stops, columns=['STOPPOINTID'])
+    return peak
+
+
+def direction(start, end, stops):
+
+    """ deciphers which direction the journey is taking place and returns the relevant key
+    such that the dictionary can be accessed """
+
+    for key, value in stops.items():
+        if start in value and end in value:
+            return key
+
+
+def prediction(route, hour, day, month, start_stop_id, end_stop_id, wind_speed, temp, humidity, weather_main,
+               stops):
+
+    journey_stops = stops[direction(start_stop_id, end_stop_id, stops)]
+    stop_sequence = journey_stops[journey_stops.index(start_stop_id):journey_stops.index(end_stop_id) + 1]
+
+    print(stop_sequence)
+    prediction_df = pd.DataFrame(stop_sequence, columns=['STOPPOINTID'])
     prediction_df['NEXT_STOPPOINTID'] = prediction_df['STOPPOINTID'].shift(-1)
     prediction_df = prediction_df.dropna(axis=0)
     prediction_df['NEXT_STOPPOINTID'] = prediction_df['NEXT_STOPPOINTID'].astype('int')
 
-    df = pd.DataFrame(columns=['DAY', 'HOUR', 'MONTH', 'temp', 'weather_main'])
-    user_input = [day, hour, month, temp, weather_main]
+    df = pd.DataFrame(columns=['DAY', 'HOUR', 'MONTH', 'PEAK_HOUR', 'wind_speed', 'temp', 'humidity', 'weather_main'])
+
+    user_input = [day, hour, month, peak_hour(hour), wind_speed, temp, humidity, weather_main]
+
     df.loc[0] = user_input
 
     frames = [prediction_df, df]
@@ -38,12 +73,15 @@ def prediction(hour, day, month, start_stop_id, end_stop_id, temp, weather_main,
     result['DAY'] = result['DAY'].astype('category')
     result['HOUR'] = result['HOUR'].astype('category')
     result['MONTH'] = result['MONTH'].astype('category')
+    result['PEAK_HOUR'] = result['PEAK_HOUR'].astype('int')
+    result['wind_speed'] = result['wind_speed'].astype('float')
     result['temp'] = result['temp'].astype('int')
+    result['humidity'] = result['humidity'].astype('float')
     result['weather_main'] = result['weather_main'].astype('category')
 
     df_dummies = pd.get_dummies(result)
 
-    with open(filePath('Test_dummies_Model_route_104.pkl'), 'rb') as handle:
+    with open(filePath('LinRegPickles/LinReg_route_{}_headers.pkl'.format(route)), 'rb') as handle:
         dummies = pickle.load(handle)
     full_dummies = pd.DataFrame(columns=dummies)
 
@@ -51,7 +89,7 @@ def prediction(hour, day, month, start_stop_id, end_stop_id, temp, weather_main,
 
     final_df_for_predict = df_b.reindex(full_dummies.columns, axis=1)
 
-    with open(filePath('Test_Model_route_104.pkl'), 'rb') as handle:
+    with open(filePath('LinRegPickles/LinReg_Model_route_{}.pkl'.format(route)), 'rb') as handle:
         model = pickle.load(handle)
 
     y_pred_linear = model.predict(final_df_for_predict)
