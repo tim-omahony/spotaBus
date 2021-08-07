973af5a9b69f8145c03503a9f14ee006126c8b0d
CianFitzgerald <cian.fitzgerald97@gmail.com>
2021-08-07 13:21:55 +0100

    Cian/ added instructions text to journey planner output

--- dublinbusapplication/Static/prediction_request.js
+++ dublinbusapplication/Static/prediction_request.js
@@ -1,5 +1,3 @@
-
-
 // function to return provided details on travel to the prediction model
 $(document).ready(function () {
     $('#form').on('submit', function (e) {
@@ -24,13 +22,68 @@ $(document).ready(function () {
 
             // if the function properly sends data to the predictive model the estimated travel time is returned
             success: function (result) {
-                console.log(result)
-                $('#output').html("<p>Estimated Bus Journey Time: " + result + " minutes</p>");
+                console.log(steps_array)
+                $('#output').html("<p>Instructions:" + results_display(Journey_Steps) + "</p>" +
+                    "<p>Estimated Bus Journey Time: " + result + " minutes</p>"
+                    );
             },
 
             failure: function (result) {
                 console.log(result)
+
+
             }
         })
     });
 })
+
+
+function results_display(array) {
+
+    const hello = JSON.parse(array)
+
+    console.log(hello)
+    var journey_instructions = `<div class="vertical-timeline vertical-timeline--animate vertical-timeline--one-column">`;
+
+    hello.forEach(function (step) {
+        console.log('step', step)
+        if (step.transit_type == "WALKING") {
+            journey_instructions +=
+                '<div class="vertical-timeline-item vertical-timeline-element">' +
+                '<div> <span class="vertical-timeline-element-icon bounce-in"> <i class="fas fa-walking"></i> </span>' +
+                `<div class="vertical-timeline-element-content bounce-in">` +
+                `<h3 class="timeline-title">${step.instructions}</h3>` +
+                `<span class="vertical-timeline-element-date">${step.step_distance}</span>` +
+                `</div></div></div>`;
+
+        } else if (step.transit_type == "TRANSIT") {
+            journey_instructions +=
+                '<div class="vertical-timeline-item vertical-timeline-element">' +
+                '<div> <span class="vertical-timeline-element-icon bounce-in"><i class="fas fa-bus"></i> </span>' +
+                `<div class="vertical-timeline-element-content bounce-in">` +
+                `<h3 class="timeline-title">${step.instructions}</h3>` +
+                `<p>Take the ${step.route}, hop off at ${step.arrival_stop}</p>` +
+                `<span class="vertical-timeline-element-date">${step.step_distance}</span>` +
+                `</div></div></div>`;
+        }
+    });
+    journey_instructions += `</div>`;
+
+    // Add the timeline to the page
+    return journey_instructions;
+
+}
+
+/*function results_display(array) {
+
+    const hello = JSON.parse(array)
+    console.log(hello)
+    console.log(hello[0].instructions)
+    var Steps = array.length;
+    route_instructions = []
+    for (var y = 0; y <= Steps; y++) {
+           route_instructions.push(hello[y].instructions);
+    }
+    return route_instructions;
+
+}*/
