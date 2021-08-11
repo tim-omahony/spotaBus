93c530632ee96fca3e41e876a1a78f1655e63125
CianFitzgerald <cian.fitzgerald97@gmail.com>
2021-08-11 19:53:01 +0100

    Cian/ changed up the alerts for deletion of routes, still trying to figure out how to remove the remove journey button when no routes saved (don't think it's fully necessary though)

--- dublinbusapplication/static/remove_fav_route.js
+++ dublinbusapplication/static/remove_fav_route.js
@@ -1,30 +1,55 @@
-$(document).ready(function () {
-    $('#delete_btn').click(function () {
-        if (confirm("Are you sure you want to delete this routes?")) {
-            const id = [];
-            const csrf = $('input[name=csrfmiddlewaretoken]').val();
-            $(':checkbox:checked').each(function (i) {
-                id[i] = $(this).val()
-            })
-            if (id.length === 0) {
-                alert("please select route(s) to delete")
-            } else {
-                console.log(id)
-                $.ajax({
-                    url: "/delete_fav_route/",
-                    method: "POST",
-                    data: {
-                        id,
-                        csrfmiddlewaretoken: csrf
-                    },
-                    success: function (response) {
-                        for (let i = 0; i < id.length; i++) {
-                            /*  $('tr#'+id[i]+'').css('background-color');*/
-                            $('tr#' + id[i] + '').fadeOut('slow');
-                        }
-                    }
-                })
-            }
-        }
+function remove_route() {
+    const id = [];
+    const csrf = $('input[name=csrfmiddlewaretoken]').val();
+    $(':checkbox:checked').each(function (i) {
+        id[i] = $(this).val()
     })
-})
+    if (id.length === 0) {
+        swal("You gotta select one (or more) first!");
+    } else {
+        console.log(id)
+        $.ajax({
+            url: "/delete_fav_route/",
+            method: "POST",
+            data: {
+                id,
+                csrfmiddlewaretoken: csrf
+            },
+            success: function (response) {
+                for (let i = 0; i < id.length; i++) {
+                    /*  $('tr#'+id[i]+'').css('background-color');*/
+                    $('tr#' + id[i] + '').fadeOut('slow');
+                }
+            }
+        })
+    }
+}
+
+function RemoveFavRouteAlert() {
+    swal({
+        title: "You sure about this?",
+        icon: "warning",
+        buttons: true,
+        dangerMode: true,
+    })
+        .then((willDelete) => {
+            if (willDelete) {
+                swal("Favourite Route Removed!", "", "success");
+                remove_route();
+            } else {
+                swal("Pheew", "Route has not been removed!", "error");
+            }
+        });
+}
+
+
+// //still working on this
+// $(function () {
+//     var tbl = document.getElementById("tbody").rows.length;
+//     console.log(tbl)
+//     if (tbl.rows.length == 0) {
+//         document.getElementById('delete_btn').style.visibility = 'hidden';
+//     } else {
+//         document.getElementById('delete_btn').style.visibility = 'visible';
+//     }
+// })
