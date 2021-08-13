3b1f47238d60f2a67b3e5692c827334513356ed4
David Kuhestani <david.kuhestani@ucdconnect.ie>
2021-08-13 13:10:45 +0200

    added userdelete

--- dublinbusesproject/settings.py
+++ dublinbusesproject/settings.py
@@ -111,10 +111,10 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
+# static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/Static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
