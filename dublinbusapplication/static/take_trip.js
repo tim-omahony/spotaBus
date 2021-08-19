c4a35e7b9821a57612de511858bbfc43cc7a9508
CianFitzgerald <cian.fitzgerald97@gmail.com>
2021-08-19 20:43:59 +0100

    cian/ just removed some commented out code and added some comments

--- dublinbusapplication/static/take_trip.js
+++ dublinbusapplication/static/take_trip.js
@@ -1,3 +1,4 @@
+//Redirects the user to the home page with the origin and destination filled into modal
 function takeTrip(originStop, destStop) {
     location.replace(`/?originStop=${originStop}&destStop=${destStop}&openRoutePlanner=true`)
 }
